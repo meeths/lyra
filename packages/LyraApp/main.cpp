e9c48d4683f33a83164a3995c28b653d26b9cb53
Sisco Macias <meeths@gmail.com>
2023-12-16 20:45:45 +0100

    Register a Stdout Logger on lyra application

--- packages/LyraApp/main.cpp
+++ packages/LyraApp/main.cpp
@@ -1,8 +1,12 @@
 #include <Application/Windows/WindowsApplication.h>
+#include <Log/Log.h>
+#include <Log/LoggerStdout.h>
 
 int main(int, char[])
 {
-
+    const auto logger = MakeSharedPointer<lyra::LoggerStdout>();
+    lyra::Log::Instance().RegisterLogger(logger);
+    
     lyra::WindowsApplication::CreationInfo windowCreationInfo;
     windowCreationInfo.m_fullScreen = false;
     windowCreationInfo.m_mainWindowSize = { 800, 600 };
